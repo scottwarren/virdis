dd2085a70bdc15a562abaed97a273be6004ea79f
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2024-01-29 17:46:44 +0200

    chore: removes file after move

--- src/state/Player/Player.types.ts
+++
@@ -1,9 +0,0 @@
-export interface Player {
-  /** ID of the player. */
-  id: string
-  /** X,Y position of the player. */
-  position: [number, number]
-}
-
-// Allows the update of any player property, except for the ID
-export type PlayerUpdateT = Partial<Omit<Player, 'id'>>
