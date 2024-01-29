bba0bf563b40bfe4c0013b23c5a852436c525059
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2024-01-29 15:59:32 +0200

    feat: adds component to render a single player entity

---
+++ src/components/player.tsx
@@ -0,0 +1,7 @@
+/**
+ * Component that is used to render an individual "player" in the game, not controllable -- just a visual representation
+ * of the item pinging around the screen
+ */
+export function Player() {
+  return <div className='h-4 w-4 rounded-full bg-green-800' />
+}
