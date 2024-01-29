3632f3f1d53f661014fa0c6482ec941023269449
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2024-01-29 17:46:48 +0200

    chore: rename

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import { Loop } from '@/components/game/Loop'
+import { GameLoop } from '@/components/game/GameLoop'
+import { useFrameTime } from '@/state/GameLoop/GameLoop.hook'
 import { GameLoopProvider } from '@/state/GameLoop/GameLoopProvider'
 
 export default function Home() {
@@ -6,10 +7,11 @@ export default function Home() {
 }
 
 function Page() {
+  const frameTime = useFrameTime()
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='max-h-screen w-full max-w-5xl items-center justify-center align-middle font-mono text-sm lg:flex'>
-        <Loop />
+        <GameLoop frameTime={frameTime} />
       </div>
     </main>
   )
