1de6fd983223378320f129f11724d2449c4ad76c
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2024-01-29 16:03:53 +0200

    feat: adds two versions of the player

--- src/components/player.tsx
+++ src/components/player.tsx
@@ -2,6 +2,23 @@
  * Component that is used to render an individual "player" in the game, not controllable -- just a visual representation
  * of the item pinging around the screen
  */
-export function Player() {
-  return <div className='h-4 w-4 rounded-full bg-green-800' />
+export function Player({ alt = false }: ComponentProps) {
+  if (alt) {
+    return <div className={`${BASE_CLASSES} ${ALT_VERSION_CLASSES}`} />
+  }
+
+  return <div className={`${BASE_CLASSES} ${REGULAR_VERSION_CLASSES}`} />
+}
+
+const BASE_CLASSES = 'h-4 w-4 rounded-full'
+
+// Classes that style the "default" version of the component
+const REGULAR_VERSION_CLASSES = 'bg-green-800'
+
+// Classes that style the "alt" version of the component
+const ALT_VERSION_CLASSES = 'bg-green-200'
+
+interface ComponentProps {
+  /** Whether or not to render the alt version of the component */
+  alt: boolean
 }
