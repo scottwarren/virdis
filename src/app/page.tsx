c3dea683a48cd73a80c9a0cf146ef98dba6e0418
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2024-01-29 18:06:34 +0200

    fix: fixes server component by default

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
+'use client'
+
 import { World } from '@/components/World/World'
-import { GameState } from '@/components/game/GameState'
-import { useFrameTime } from '@/state/GameState/GameState.hook'
 import { GameStateProvider } from '@/state/GameState/GameStateProvider'
 
 export default function Home() {
