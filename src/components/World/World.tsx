bd262c584c8020c7d0eeaeff6157d3ec9bb2d279
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2024-02-07 07:50:27 +0200

    feat: started rendering the blocks to the world.

--- src/components/World/World.tsx
+++ src/components/World/World.tsx
@@ -1,9 +1,10 @@
 import { useGameState } from '@/state/GameState/GameStateProvider.hooks'
 import { Player } from '../Player/Player'
 import { useFrameTime, useGameLoop } from './World.hooks'
+import { Block } from '@/components/Block/Block'
 
 export function World() {
-  const { players, setGamePaused, gamePaused } = useGameState()
+  const { blocks, players, setGamePaused, gamePaused } = useGameState()
   const frameTime = useFrameTime()
   useGameLoop(frameTime)
 
@@ -12,6 +13,10 @@ export function World() {
       {players.map((player) => {
         const [x, y] = player.position
         return <Player key={player.id} x={x} y={y} />
+      })}
+      {blocks.map((block) => {
+        const [x, y] = block.position
+        return <Block key={block.id} x={x} y={y} />
       })}
       <div className='flex flex-row justify-end'>
         <button
