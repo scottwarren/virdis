4b55801db18ddb258bfffe34265b43aa8d74e661
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2024-02-07 07:27:30 +0200

    chore: removes the previously extracted constants

--- src/components/Player/Player.types.ts
+++ src/components/Player/Player.types.ts
@@ -9,11 +9,8 @@ export interface PlayerI {
   velocity: [number, number]
 }
 
-// Amount of movement applied to the player per tick
-export const DEFAULT_PLAYER_VELOCITY_PER_TICK = 2
 // For security reasons we don't allow the user to update the ID.
 type ValidFieldsToUpdateOnPlayer = Omit<PlayerI, 'id'>
 
-export const DEFAULT_PLAYER_SIZE_PX = 16
 // Allows the update of any player property, except for the ID
 export type PlayerUpdateT = Partial<ValidFieldsToUpdateOnPlayer>
