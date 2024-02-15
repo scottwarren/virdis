5f9674b7d3b1a87406ea9ba73e75cf93975af982
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2024-02-15 15:27:17 +0200

    feat: generates a set of "random" blocks

--- src/models/Block/Block.ts
+++ src/models/Block/Block.ts
@@ -17,7 +17,7 @@ export function generateBlocks(numberOfBlocks: number) {
   const blocks: BlockI[] = []
 
   for (let i = 0; i < numberOfBlocks; i++) {
-    const position: BlockI['position'] = [450, 480]
+    const position: BlockI['position'] = generatePseudoRandomPosition()
 
     const newBlock: BlockI = {
       id: uuidv4(),
@@ -28,4 +28,36 @@ export function generateBlocks(numberOfBlocks: number) {
   }
 
   return blocks
+}
+
+export function generatePseudoRandomPosition(): BlockI['position'] {
+  // We don't want the blocks to be generated outside of the screen
+  // therefore we use the block size to calculate the maximum X and Y
+  const MAX_BLOCK_X = document.body.clientWidth - DEFAULT_BLOCK_SIZE
+  const MAX_BLOCK_Y = document.body.clientHeight - DEFAULT_BLOCK_SIZE
+
+  // Using the min of DEFAULT_BLOCK_SIZE for the same reason as above,
+  // We don't want the blocks to be generated outside of the screen
+  const x = randomIntFromInterval(DEFAULT_BLOCK_SIZE, MAX_BLOCK_X)
+  const y = randomIntFromInterval(DEFAULT_BLOCK_SIZE, MAX_BLOCK_Y)
+
+  console.log('x', x, 'y', y)
+
+  return [x, y]
+}
+
+/**
+ * Function to generate a random integer between min and max.
+ *
+ * @returns a random integer between min and max (inclusive of min and max)
+ *
+ * @see https://stackoverflow.com/a/7228322
+ */
+function randomIntFromInterval(min: number, max: number) {
+  // Rouding up the min, otherwise we could get a number less than min
+  // same for max, we round down to avoid getting a number greater than max
+  const minInt = Math.ceil(min)
+  const maxInt = Math.floor(max)
+
+  return Math.floor(Math.random() * (max - min + 1) + min)
 }
