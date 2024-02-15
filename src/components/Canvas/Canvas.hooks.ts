2ca6c8ba9cd500609268b343e00b2a1178596cb3
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2024-02-15 19:46:12 +0200

    feat: adds fit canvas to screen hook

--- src/components/Canvas/Canvas.hooks.ts
+++ src/components/Canvas/Canvas.hooks.ts
@@ -20,5 +20,24 @@ export function useClearCanvas(canvasRef: React.RefObject<HTMLCanvasElement>) {
       width: canvas.width,
       height: canvas.height,
     })
-  }, [])
+  }, [canvasRef])
+}
+
+/**
+ * Hook used to fit the canvas to the screen.
+ */
+export function useFitCanvasToScreen(
+  canvasRef: React.RefObject<HTMLCanvasElement>,
+) {
+  useEffect(() => {
+    const canvas = canvasRef.current
+
+    if (!canvas) {
+      return
+    }
+
+    // Ensure the canvas is the same size as the window
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+  }, [canvasRef])
 }
