7eebb999f2bd3ca3070c5467e1bc06c9b7ec5329
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2024-02-15 15:05:41 +0200

    chore: adds more tests

--- src/components/World/World.helpers.spec.ts
+++ src/components/World/World.helpers.spec.ts
@@ -8,6 +8,17 @@ describe('World.helpers', () => {
     })
     it('should return false is not within the area', () => {
       expect(blockHitDetection([452, 452], [450, 480])).toEqual(false)
+      expect(blockHitDetection([0, 0], [450, 480])).toEqual(false)
+      expect(blockHitDetection([1000, 460], [450, 480])).toEqual(false)
+    })
+
+    it('should return false if the playerY is within the block, but not the X', () => {
+      expect(blockHitDetection([100, 490], [450, 480])).toEqual(false)
+      expect(blockHitDetection([1000, 490], [450, 480])).toEqual(false)
+    })
+    it("should return false if the player's X is within the block, but not the Y", () => {
+      expect(blockHitDetection([460, 1000], [450, 480])).toEqual(false)
+      expect(blockHitDetection([460, 0], [450, 480])).toEqual(false)
     })
   })
 })
