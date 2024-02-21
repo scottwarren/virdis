944689f527c2e15b2b1cf4c86ad62f70e5bf46cb
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2024-02-21 16:13:39 +0200

    fix: fixes circles being drawn from the center instead of top left
    this meant that the collision detection for the boundary of the left/top wall was not being detected as a hit until the center of the circle was past the top/bottom

--- src/components/Player/Player.helpers.ts
+++ src/components/Player/Player.helpers.ts
@@ -10,17 +10,27 @@ export function drawPlayer(context: CanvasRenderingContext2D, player: PlayerI) {
 
   context.beginPath()
 
-  context.arc(
-    x,
-    y,
-    DEFAULT_PLAYER_SIZE_PX,
-    CIRCLE_START_ANGLE,
-    CIRCILE_END_ANGLE,
-  )
+  // By default the arc is drawn from the centre of the x, y, which makes collision detection harder. We want the arc to be drawn from the top left.
+  const circleX = x - DEFAULT_PLAYER_SIZE_PX / 2
+  const circleY = y - DEFAULT_PLAYER_SIZE_PX / 2
+
+  arcTopLeft(context, circleX, circleY, DEFAULT_PLAYER_SIZE_PX)
 
   context.fill()
 }
 
-// Start/end angle for a circle, used to draw the player.
-const CIRCLE_START_ANGLE = 0
-const CIRCILE_END_ANGLE = Math.PI * 2
+/**
+ * Function used to draw an arc/circle within the canvas, starting from the top left instead of from the centre.
+ *
+ * Angle parameters default to draw a circle (0, Math.PI * 2).
+ */
+function arcTopLeft(
+  context: CanvasRenderingContext2D,
+  x: number,
+  y: number,
+  radius: number,
+  startAngle = 0,
+  endAngle = Math.PI * 2,
+) {
+  context.arc(x + radius, y + radius, radius, startAngle, endAngle, false)
+}
